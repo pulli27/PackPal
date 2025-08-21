20b35cc21408d4c3a4250d4657d1e7120a6518c9
pulli27 <pulmivihansa27@gmail.com>
2025-08-21 10:13:15 +0530

    update

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,1 +1,18 @@
-console.log("hi");
+//pass= H1234pul
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Middleware
+app.use("/",(req, res, next) => {
+    res.send("It is Working");
+})
+
+mongoose.connect("mongodb+srv://pulmivihansa27:H1234pul@cluster0.uowmnpn.mongodb.net/")
+.then(()=> console/log("Connected to MongoDB"))
+.then(()=> {
+    app.listen(5000);
+})
+.catch((err)=> console.log((err)));
