1e254270363f7ae9cb65fff9d1d10723655e545e
sasangihasadara <sasangihasadara@gmail.com>
2025-08-25 19:44:14 +0530

    update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -43,9 +43,11 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Connect to MongoDB
-mongoose.connect("mongodb+srv://pulmivihansa27:H1234pul@cluster0.uowmnpn.mongodb.net/")
-.then(() => console.log("Connected to MongoDB"))
-.then(() => {
-    app.listen(5000);/* () => console.log("Server running on port 5000"));*/
-})
-.catch((err) => console.log(err));
+const MONGO_URI = "mongodb+srv://pulmivihansa27:H1234pul@cluster0.jhfjlg0.mongodb.net/PackPal?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+  })
+  .catch((err) => console.error("Mongo connect error:", err));
