57198659ce9cf6cbc981fe49c23409f5c2dd70ee
isumi841 <IT23542938@my.sliit.lk>
2025-08-28 18:19:53 +0530

    update

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -12,9 +12,11 @@ const app = express();
 app.use("/users", router);
 
 
-mongoose.connect("mongodb+srv://pulmivihansa27:H1234pul@cluster0.uowmnpn.mongodb.net/")
-.then(()=> console.log("Connected to MongoDB"))
-.then(()=> {
-    app.listen(5000);
-})
-.catch((err)=> console.log((err)));
+const MONGO_URI = "mongodb+srv://pulmivihansa27:H1234pul@cluster0.2pgj0bt.mongodb.net/PackPal?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+  })
+  .catch((err) => console.error("Mongo connect error:", err));
