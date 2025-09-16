2ced5319120706fe1c0f039634045ae318b37ece
sasangihasadara <sasangihasadara@gmail.com>
2025-09-16 11:27:24 +0530

    change

--- FrontEnd/packpal/src/components/Sidebar/Sidebar.js
+++ FrontEnd/packpal/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react"
+/*import React from "react"
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
 
@@ -28,3 +28,4 @@ function Sidebar()  {
 };
 
 export default Sidebar;
+*/
